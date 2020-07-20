b4b7051eb5728f1a4caa931e53b6cf821f53611b
Jocomol <joelmeier08@gmail.com>
2020-07-20 11:16:23 +0200

    - added JavaDoc and Comments - added back the errorhandling - removed .gitattributes - updated to java 14 - added versions in the pom

--- src/main/java/BatchPDFSign/BatchPDFSign.java
+++ src/main/java/BatchPDFSign/BatchPDFSign.java
@@ -11,15 +11,20 @@ import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 
-
+/**
+ * Signs PDF files and retains PDF/A conformity if wanted.
+ * @author Pep Marxuach, jmarxuach
+ * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+ * @version 1.0.2
+ */
 public class BatchPDFSign {
 
 	private static PrivateKey privateKey;
 	private static Certificate[] certificateChain;
 
-	private static String PRODUCTNAME = "BatchPDFSign";
-	private static String VERSION = "version 1.0.1";
-	private static String JAR_FILENAME = "BatchPDFSign.jar";
+	private static final String PRODUCTNAME = "BatchPDFSign";
+	private static final String VERSION = "version 1.0.2";
+	private static final String JAR_FILENAME = "BatchPDFSign.jar";
 
 	private final String pkcs12FileName;
 	private final String PkcsPassword;
@@ -33,6 +38,11 @@ public class BatchPDFSign {
 
 	private final File inputFile;
 
+	/**
+	 * Constructor for when the jar is executed directly.
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param args arguments received from the commandline.
+	 */
 	public BatchPDFSign(String[] args){
 		this(args[0], args[1], args[2]);
 		this.flgRename = !(args.length >= 4);
@@ -40,13 +50,21 @@ public class BatchPDFSign {
 		if(flgRename){
 			this.pdfOutputFileName = pdfInputFileName + "-sig";
 		} else {
-			this.setPdfOutputFileName(args[3]);
+			this.pdfOutputFileName = args[3];
 		}
 		if(this.pdfA){
 			this.setPDFA(args[4]);
 		}
 	}
 
+	/**
+	 * The most basic constructor, only initializes the file name, the file password and the pdf which should be signed class variable.
+	 * This constructor is called by every other constructor.
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param pkcs12FileName File name of the key.
+	 * @param PkcsPassword Password of the key.
+	 * @param pdfInputFileName File name of the PDF file which should be signed.
+	 */
 	public BatchPDFSign(String pkcs12FileName, String PkcsPassword, String pdfInputFileName){
 		this.pkcs12FileName = pkcs12FileName;
 		this.PkcsPassword = PkcsPassword;
@@ -56,49 +74,66 @@ public class BatchPDFSign {
 		this.flgRename = true;
 	}
 
+	/**
+	 * The constructor which is called if the PDF file which is signed, shouldn't be overwritten.
+	 * This constructor calls the basic constructor.
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param pkcs12FileName File name of the key.
+	 * @param PkcsPassword Password of the key.
+	 * @param pdfInputFileName File name of the PDF file which should be signed.
+	 * @param pdfOutputFileName File name which the signed PDF should have.
+	 */
 	public BatchPDFSign(String pkcs12FileName, String PkcsPassword, String pdfInputFileName, String pdfOutputFileName){
 		this(pkcs12FileName, PkcsPassword, pdfInputFileName);
 		this.pdfOutputFileName = pdfOutputFileName;
 		this.flgRename = false;
 	}
 
+	/**
+	 * The PDF/A Constructor, only works when a output file is defined aswell.
+	 * This constructor calls the (String, String, String, String) constructor.
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param pkcs12FileName File name of the key.
+	 * @param PkcsPassword Password of the key.
+	 * @param pdfInputFileName File name of the PDF file which should be signed.
+	 * @param pdfOutputFileName File name of the PDF file which should be signed.
+	 * @param pdfAFormat Name of the PDF/A format.
+	 */
 	public BatchPDFSign(String pkcs12FileName, String PkcsPassword, String pdfInputFileName, String pdfOutputFileName, String pdfAFormat){
 		this(pkcs12FileName, PkcsPassword, pdfInputFileName, pdfOutputFileName);
 		this.pdfA = true;
 		this.setPDFA(pdfAFormat);
 	}
 
+	/**
+	 * Sets the class variable pdfAConformanceLevel to the conformance level defined in the parameter pdfAFormat.
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param pdfAFormat Name of the conformance level the class variable pdfAConformanceLevel should be set to.
+	 */
 	public void setPDFA(String pdfAFormat){
-		switch (pdfAFormat){
-			case "PDF_A_1A": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_1A;
-				break;
-			case "PDF_A_1B": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_1B;
-				break;
-			case "PDF_A_2A": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_2A;
-				break;
-			case "PDF_A_2B": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_2B;
-				break;
-			case "PDF_A_2U": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_2U;
-				break;
-			case "PDF_A_3A": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_3A;
-				break;
-			case "PDF_A_3B": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_3B;
-				break;
-			case "PDF_A_3U": this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_3U;
-				break;
-			case "ZUGFeRD": this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRD;
-				break;
-			case "ZUGFeRDBasic": this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRDBasic;
-				break;
-			case "ZUGFeRDComfort": this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRDComfort;
-				break;
-			case "ZUGFeRDExtended": this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRDExtended;
-				break;
-			default: throw new IllegalArgumentException();
+		switch (pdfAFormat) {
+			case "PDF_A_1A" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_1A;
+			case "PDF_A_1B" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_1B;
+			case "PDF_A_2A" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_2A;
+			case "PDF_A_2B" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_2B;
+			case "PDF_A_2U" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_2U;
+			case "PDF_A_3A" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_3A;
+			case "PDF_A_3B" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_3B;
+			case "PDF_A_3U" -> this.pdfAConformanceLevel = PdfAConformanceLevel.PDF_A_3U;
+			case "ZUGFeRD" -> this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRD;
+			case "ZUGFeRDBasic" -> this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRDBasic;
+			case "ZUGFeRDComfort" -> this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRDComfort;
+			case "ZUGFeRDExtended" -> this.pdfAConformanceLevel = PdfAConformanceLevel.ZUGFeRDExtended;
+			default -> throw new IllegalArgumentException();
 		}
 	}
 
-	public void signFile() throws GeneralSecurityException, IOException, DocumentException {
+	/**
+	 * Signs a PDF file. This method is configured by the constructors of this class.
+	 * @author Pep Marxuach, jmarxuach
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 */
+	public void signFile() {
 		try{
 			// Check PDF input file
 			if (!inputFile.exists() || inputFile.isDirectory()) {
@@ -130,21 +165,61 @@ public class BatchPDFSign {
 				} else {
 					throw new FileNotFoundException();
 				}
-
 			}
-
-		} catch (FileNotFoundException e){
-			showUsage();
+		} catch (IOException | DocumentException e){
+			errorHandling(e, "The PDF file wasn't found or wasn't readable. Please check if you entered the correct file, if it exists and if the permissions are set correctly.", false, true);
+		} catch (GeneralSecurityException e) {
+			errorHandling(e, "A fatal error occurred, please contact the developer with the following details:", true, false);
 		}
 	}
 
-	protected static void readPrivateKeyFromPKCS12 (String pkcs12FileName, String pkcs12Password) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
-		KeyStore ks = KeyStore.getInstance("pkcs12");
-		ks.load(new FileInputStream(pkcs12FileName), pkcs12Password.toCharArray());
-		String alias = ks.aliases().nextElement();
-		privateKey = (PrivateKey) ks.getKey(alias, pkcs12Password.toCharArray());
-		certificateChain = ks.getCertificateChain(alias);
+	/**
+	 * Loads the signer key from the file.
+	 * @author Pep Marxuach, jmarxuach
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param pkcs12FileName File name of the key file.
+	 * @param pkcs12Password Password to unlock the key file.
+	 */
+	protected static void readPrivateKeyFromPKCS12 (String pkcs12FileName, String pkcs12Password)  {
+		try {
+			KeyStore ks = KeyStore.getInstance("pkcs12");
+			ks.load(new FileInputStream(pkcs12FileName), pkcs12Password.toCharArray());
+			String alias = ks.aliases().nextElement();
+			privateKey = (PrivateKey) ks.getKey(alias, pkcs12Password.toCharArray());
+			certificateChain = ks.getCertificateChain(alias);
+		}
+		catch (UnrecoverableKeyException e){
+			errorHandling(e, "The Key could not be recovered. The password was probably entered incorrectly", false, true);
+		} catch (KeyStoreException | CertificateException | NoSuchAlgorithmException e){
+			errorHandling(e, "Key has an invalid signature. Please check if you entered the correct file.", false, true);
+		} catch (IOException e) {
+			errorHandling(e, "Key file wasn't found or wasn't readable. Please check if you entered the correct file, if it exists and if the permissions are set correctly.", false, true);
+		}
 	}
+
+	/**
+	 * Handles errors. can print stacktrace, usage and errormessages.
+	 * This Method is configurable by booleans.
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 * @param e Exception.
+	 * @param errorMessage Error message for the user.
+	 * @param printStacktrace Should it print the stacktrace?
+	 * @param showUsage Should it call the showUsage() method?
+	 */
+	public static void errorHandling(Exception e, String errorMessage, Boolean printStacktrace, Boolean showUsage){
+		System.err.println(errorMessage);
+		if (e != null && printStacktrace){
+			e.printStackTrace();
+		}
+		if (showUsage){
+			showUsage();
+		}
+	}
+	/**
+	 * Basic help and usage documentation.
+	 * @author Pep Marxuach, jmarxuach
+	 * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+	 */
 	public static void showUsage() { //TODO Update
 		System.out.println("jPdfSign v" + VERSION + " \n");
 		System.out.println(PRODUCTNAME + " usage:");
@@ -152,10 +227,5 @@ public class BatchPDFSign {
 		System.out.print("\tjava -jar " + JAR_FILENAME);
 		System.out.println(" <pkcs12FileName> <pkcsPassword> <pdfInputFileName> <pdfOutputFileName>");
 		System.out.println("\n<pdfOutputFileName> is optional");
-		System.exit(0);
-	}
-
-	private void setPdfOutputFileName(String pdfOutputFileName) {
-		this.pdfOutputFileName = pdfOutputFileName;
 	}
 }
--- src/main/java/BatchPDFSign/Main.java
+++ src/main/java/BatchPDFSign/Main.java
@@ -1,25 +1,18 @@
 package BatchPDFSign;
 
-import com.itextpdf.text.DocumentException;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
+/**
+ * This method is called when the jar is executed. It shows the help if fewer than 3 args were entered.
+ * If more than 3 args were entered, it creates a BatchPDFSign Object and calls the signFile(); function on it.
+ * @author Joe Meier, Jocomol, joelmeier08@gmail.com
+ * @version 1.0.2
+ */
 public class Main {
     public static void main(String[] args){
-        if (args.length < 3){
+        if (args.length < 3){ //Shows help if not enough args were entered
             BatchPDFSign.showUsage();
         } else {
             BatchPDFSign batchPDFSign = new BatchPDFSign(args);
-            try {
-                batchPDFSign.signFile();
-            } catch (GeneralSecurityException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (DocumentException e) {
-                e.printStackTrace();
-            }
+            batchPDFSign.signFile();
         }
     }
 }
